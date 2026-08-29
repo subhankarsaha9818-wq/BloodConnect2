2b698130e19c91fd1b8167e762bce69f6ae91009
Subhankar <subhankarsaha9818@gmail.com>
2026-08-29 21:29:01 +0530

    Backend problem solved

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -1,6 +1,6 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-console.log("Gemini Key:", process.env.GEMINI_API_KEY);
+console.log("Gemini Key loaded:", !!process.env.GEMINI_API_KEY);
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
 const newsletterRoutes = require("./routes/newsletterRoutes");
 require("dotenv").config();
+
 
 const chatRoutes = require("./routes/chatRoutes");
 const express = require("express");
